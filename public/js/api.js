20b5f3296454f3dc8acba7c1eb991aa9126975bf
Jackson Rios <juniorrios1231@gmail.com>
2022-12-12 09:53:23 -0300

    clone-netflix

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key ='c12e0ab6f852fc626d24ab7d9cb78646'
+let img_url ='https://image.tmdb.org/t/p/w500'
+let genres_list_http='https://api.themoviedb.org/3/genre/movie/list?'
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+let movie_videos_url='https://api.themoviedb.org/3/movie/{movie_id}/videos?'
